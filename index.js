607b8493d8ccebfcd456ccf322f2f947f4350b81
Brock <zyuan18@my.bcit.ca>
2023-11-20 15:46:27 -0800

    Added auth/login

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const session = require("express-session");
 const ejsLayouts = require("express-ejs-layouts");
 const reminderController = require("./controller/reminder_controller");
 const authController = require("./controller/auth_controller");
+const passport = require("./middleware/passport");
+
 
 app.use(express.static(path.join(__dirname, "public")));
 
@@ -24,13 +27,60 @@ app.post("/reminder/update/:id", reminderController.update);
 app.post("/reminder/delete/:id", reminderController.delete);
 
 // 👌 Ignore for now
-app.get("/register", authController.register);
-app.get("/login", authController.login);
-app.post("/register", authController.registerSubmit);
-app.post("/login", authController.loginSubmit);
+// app.get("/register", authController.register);
+// app.get("/login", authController.login);
+// app.post("/register", authController.registerSubmit);
+// app.post("/login", authController.loginSubmit);
 
 app.listen(3001, function () {
   console.log(
     "Server running. Visit: http://localhost:3001/reminders in your browser 🚀"
   );
+});
+
+
+app.use(
+  session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+      maxAge: 24 * 60 * 60 * 1000,
+    },
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use((req, res, next) => {
+  console.log(`User details are: `);
+  console.log(req.user);
+
+  console.log("Entire session object:");
+  console.log(req.session);
+
+  console.log(`Session details are: `);
+  console.log(req.session.passport);
+  next();
+});
+
+app.get(
+  "/auth/login", (req, res) => {
+    res.render("auth/login")
+  }
+);
+
+app.post(
+  "/auth/login",
+  passport.authenticate("local", {
+    successRedirect: "/dashboard",
+    failureRedirect: "/auth/login",
+  })
+);
+
+app.get("/auth/logout", (req, res) => {
+  req.logout();
+  res.redirect("/auth/login");
 });
--- models/userModel.js
+++ models/userModel.js
@@ -1,39 +1,21 @@
-// let Database = {
-//   cindy: {
-//     reminders: [
-//       {
-//         id: 1,
-//         title: "Grocery shopping",
-//         description: "Buy milk and bread from safeway",
-//         completed: false,
-//       },
-//     ],
-//   },
-// };
-
-// module.exports = Database;
-
 const database = [
   {
     id: 1,
     name: "Jimmy Smith",
     email: "jimmy123@gmail.com",
     password: "jimmy123!",
-    reminders:[],
   },
   {
     id: 2,
     name: "Johnny Doe",
     email: "johnny123@gmail.com",
     password: "johnny123!",
-    reminders:[],
   },
   {
     id: 3,
     name: "Jonathan Chen",
     email: "jonathan123@gmail.com",
     password: "jonathan123!",
-    reminders:[],
   },
 ];
 
